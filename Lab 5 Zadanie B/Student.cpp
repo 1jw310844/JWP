8c896b46e8374045ff3cb3e1008a8ac8810f5391
1jw310844 <149474616+1jw310844@users.noreply.github.com>
2025-04-07 14:26:20 +0200

    Update Student.cpp

--- Lab 5 Zadanie B/Student.cpp
+++ Lab 5 Zadanie B/Student.cpp
@@ -1,28 +1,41 @@
+using System.ComponentModel.Design;
+
 namespace Generyki
 {
-    public class Student
+    public class Student : IComparable<Student>
     {
-        private double numerAlbumu;
-        public double NumerAlbumu {
-            get {return numerAlbumu; }
-            set {numerAlbumu=value; } 
-        }
-        private string nazwisko;
-        public string Nazwisko {
-            get {return nazwisko; }
-            set {nazwisko = value; } 
-        }
-        private double ocena;
-        public double Ocena {
-            get {return ocena; }
-            set {ocena = value; } 
-        }
+        public double NumerAlbumu { get; set; }
+        public string Nazwisko { get; set; }
+        public double Ocena { get; set; }
 
         public Student(double na, string name, double oc)
         {
-            this.numerAlbumu = na;
-            this.ocena = oc;
-            this.nazwisko = name;
+            this.NumerAlbumu = na;
+            this.Ocena = oc;
+            this.Nazwisko = name;
+        }
+
+        // Porównujemy studentów wg oceny
+        public int CompareTo(Student? other)
+        {
+            if (other == null) return 1;
+            return this.Ocena.CompareTo(other.Ocena);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nazwisko} ({NumerAlbumu}), Ocena: {Ocena}";
+        }
+    }
+
+    public static class Wiekszy
+    {
+        public static T ZnajdzWiekszy<T>(T a, T b) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) >= 0)
+                return a;
+            else
+                return b;
         }
     }
 }
