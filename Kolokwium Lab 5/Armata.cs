8495000c04b20bc2bc793ef84fd4d937b5d2c652
1jw310844 <149474616+1jw310844@users.noreply.github.com>
2025-04-14 12:44:02 +0200

    Add files via upload

---
+++ Kolokwium Lab 5/Armata.cs
@@ -0,0 +1,28 @@
+﻿namespace Klasy
+{
+    public class Armata
+    {
+        private double kaliber;
+        private double masa;
+        public double Kaliber
+        {
+            get { return kaliber; }
+            set { kaliber = value; }
+        }
+        public double Masa
+        {
+            get { return masa; }
+            set { masa = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Kaliber: {Kaliber}, Masa: {Masa} kg";
+        }
+        public Armata(double k, double m)
+        {
+            masa = m;
+            kaliber = k;
+        }
+    }
+}
---
+++ Kolokwium Lab 5/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Klasy;
+
+namespace Kolokwium_Wesoły
+{
+    public partial class MainWindow : Window
+    {
+        private static Random random = new Random();
+        public static T JedenZTrzech<T>(T a, T b, T c)
+        {
+            int wylosowana = random.Next(3);
+            return wylosowana switch
+            {
+                0 => a,
+                1 => b,
+                _ => c
+            };
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonString_Click(object sender, RoutedEventArgs e)
+        {
+            string pierwszy = "Języki";
+            string drugi = "Wysokiego";
+            string trzeci = "Poziomu";
+            string wynik = JedenZTrzech(pierwszy, drugi, trzeci);
+            MessageBox.Show($"Wylosowany string: {wynik}");
+        }
+
+        private void ButtonArmata_Click(object sender, RoutedEventArgs e)
+        {
+            Armata a1 = new Armata(100, 3000);
+            Armata a2 = new Armata(120, 4000);
+            Armata a3 = new Armata(150, 5000);
+
+            Armata losowa = JedenZTrzech(a1, a2, a3);
+            
+            MessageBox.Show($"Wylosowana armata:\n{losowa}");
+        }
+    }
+    
+}
