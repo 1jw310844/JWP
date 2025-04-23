516bd646559bcfa171f609c50f26511668cb0544
1jw310844 <149474616+1jw310844@users.noreply.github.com>
2025-04-23 11:12:19 +0200

    Add files via upload

---
+++ Kolokwium Lab 6/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Klasy;
+/// Grupa A
+
+
+namespace Kolokwium_6_Wesoły
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Kula kula1 = new Kula(1, "stal");
+            Kula kula2 = new Kula(1, "drewno");
+
+            kula1.ZmienPromien(p => p + 2);
+            kula2.ZmienPromien(p => p * 3);
+
+            lstLista.Items.Clear();
+            lstLista.Items.Add(kula1.ToString());
+            lstLista.Items.Add(kula2.ToString());
+        }
+    }
+    
+}
---
+++ Kolokwium Lab 6/klasy.cs
@@ -0,0 +1,48 @@
+﻿namespace Klasy
+{
+    public class Kula
+    {
+        private double promien;
+        public double Promien
+        {
+            get { return promien; }
+            private set { promien = value; }
+        }
+        private string material;
+        public string Material 
+        {
+            get { return material; }
+            set { material = value; } 
+        }
+
+        public Kula(double pr, string mat)
+        {
+            Promien = pr;
+            Material = mat;
+        }
+
+        public void ZmienPromien(Func<double, double> zmiana)
+        {
+            Promien = zmiana(Promien);
+        }
+
+        public double Masa()
+        {
+            double gestosc = Material.ToLower() switch
+            {
+                "stal" => 7850,
+                "drewno" => 700,
+                _ => 1000
+            };
+
+            double objetosc = (4.0 / 3.0) * Math.PI * Math.Pow(Promien, 3);
+            double wynik = objetosc * gestosc;
+            return wynik;
+        }
+
+        public override string ToString()
+        {
+            return $"Kula z materiału: {Material}, promień: {Promien:F2}, masa: {Masa():F2} kg";
+        }
+    }
+}
